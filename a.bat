67e104fe68c140470e350a0cd4a3f698dc83af54
Carlos12345678910111213 <161318887+Carlos12345678910111213@users.noreply.github.com>
2024-02-26 13:20:05 +0100

    Add files via upload

---
+++ a.bat
@@ -0,0 +1,13 @@
+@echo off
+md MisDocumentos
+cd MisDocumentos
+md Fotos
+md Trabajos
+md Musica
+cd Fotos
+md Vacaciones
+md Familia
+echo.> texto.txt hola buenas
+echo La estructura de archivos se creo con exito
+pause
+
